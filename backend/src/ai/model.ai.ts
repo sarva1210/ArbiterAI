7c883246cc38ec93db04fc825a0e31e8baae92d2
sarva1210 <sarva.vish77@gmail.com>
2026-04-11 19:53:28 +0530

    implementing ai models

--- backend/src/ai/model.ai.ts
+++ backend/src/ai/model.ai.ts
@@ -1,6 +1,12 @@
 import { ChatGoogle } from "@langchain/google";
+import { ChatMistralAI } from "@langchain/mistralai";
 import config from "../config/config.js";
+
 export const geminiModel = new ChatGoogle({
     model: "gemini-flash-latest",
     apiKey: config.GOOGLE_API_KEY,
+})
+export const mistralAIModel = new ChatMistralAI({
+    model: "mistral-medium-latest",
+    apiKey: config.MISTRALAI_API_KEY,
 })
