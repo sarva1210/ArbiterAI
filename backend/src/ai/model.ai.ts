76894b2b40b3ee17b3c4460f0be0f992349611ef
sarva1210 <sarva.vish77@gmail.com>
2026-04-12 19:35:02 +0530

    ai integration

--- backend/src/ai/model.ai.ts
+++ backend/src/ai/model.ai.ts
@@ -7,10 +7,12 @@ export const geminiModel = new ChatGoogle({
     model: "gemini-flash-latest",
     apiKey: config.GOOGLE_API_KEY,
 })
+
 export const mistralAIModel = new ChatMistralAI({
     model: "mistral-medium-latest",
     apiKey: config.MISTRALAI_API_KEY,
 })
+
 export const cohereModel = new ChatCohere({
     model: "command-a-03-2025",
     apiKey: config.COHERE_API_KEY,
